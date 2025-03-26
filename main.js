575d0a57944b24300e73a051f649c4c7d1448ce2
Urmila111 <basneturmi777@gmail.com>
2025-03-26 21:40:02 +0545

    connected to mongodb

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 import express from "express";
-
+import mongoose from "mongoose";
 const app = express();
-app.use("/api", (req,res,next) =>{
-    res.send("Hello World")
-})
+
+mongoose
+.connect(
+    "mongodb+srv://genius:genius123@cluster0.cscvf5v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
+.then(()=>app.listen(5000))
+.then(()=>
+    console.log("Connected to database and listening to localhost 5000")
+)
+.catch((err)=> console.log(err))
+
