f1a3becb24173ad1100abfff0b2ca843daed0b0a
Urmila111 <basneturmi777@gmail.com>
2025-04-02 21:10:57 +0545

    add login routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getAllUser, signup } from "../controllers/user.controller.js";
+import { getAllUser, signup, login } from "../controllers/user.controller.js";
 
 export const router = express.Router();
 
 router.get("/", getAllUser );
 router.post("/signup", signup);
-router.post("/login", )
+router.post("/login", login);
