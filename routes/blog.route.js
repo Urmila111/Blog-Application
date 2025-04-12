2e49a39ac0a752bad5938d63d9d3080b8266e2ee
Urmila111 <basneturmi777@gmail.com>
2025-04-12 10:46:00 +0545

    added to blog route

--- routes/blog.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getAllBlogs } from "../controllers/blog.controller";
-const blogRouter = express.Router();
-
-blogRouter.get("/", getAllBlogs);
-
-export default blogRouter;
