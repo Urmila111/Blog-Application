587decdbe05062fcef1abc28128f3613ff5ed271
Urmila111 <basneturmi777@gmail.com>
2025-04-12 10:46:44 +0545

    updated main

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import {router} from "./routes/user.routes.js";
+import blogRouter from "./routes/blog.routes.js";
 const app = express();
 app.use(express.json());
 app.use("/api/user", router);
-app.use("/api/bog", blogRouter);
+app.use("/api/blog", blogRouter);
 mongoose
 .connect(
     "mongodb+srv://genius:genius123@cluster0.cscvf5v.mongodb.net/"
