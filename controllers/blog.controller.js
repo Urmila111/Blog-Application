f86ab5334fbb9e48e69b15b1b79d3d3226a21bd2
Urmila111 <basneturmi777@gmail.com>
2025-04-15 08:51:09 +0545

    update blog controller

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -28,3 +28,18 @@ export const addBlog = async(req,res,next) => {
     }
     return res.status(200).json({blog})
 };
+
+export const updateBlog = async(req,res,next) => {
+    const {title, description } = req.body
+    const blogId = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findByIdAndUpdate(blogId, {
+            title,
+            description
+          })
+        
+    } catch (error) {
+        return
+    } 
+}
