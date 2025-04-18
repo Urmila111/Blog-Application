86a95f423a32fb8af53bb46ca7cc7203010714a2
Urmila111 <basneturmi777@gmail.com>
2025-04-18 22:17:42 +0545

    blog routes updated

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getAllBlogs, addBlog, updateBlog} from "../controllers/blog.controller.js";
+import { getAllBlogs, addBlog, updateBlog, getById} from "../controllers/blog.controller.js";
 const blogRouter = express.Router();
 
 blogRouter.get("/", getAllBlogs);
 blogRouter.post("/add",addBlog);
 blogRouter.put("/update/:id",updateBlog);
-blogRouter.get("/:id",  getById)
+blogRouter.get("/:id", getById);
 export default blogRouter;
