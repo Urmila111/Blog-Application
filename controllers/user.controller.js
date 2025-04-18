b8865635b364b98d00dcb583b618bd32510e257c
Urmila111 <basneturmi777@gmail.com>
2025-04-18 22:17:22 +0545

    user controller updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -64,16 +64,4 @@ export const login = async(req, res, next )=>{
    }
    return res.status(200).json({message:"Login Successfull"})
 };
-export const getById = async(req, res, next)=> {
-   const id = req.params.id;
-   let blog;
-   try {
-      blog = await Blog.findById(id);
-   } catch (err) {
-     return console.log(err);
-   }
-   if (!blog) {
-     return res.status(404).json({message: "No Blog Found"})
-   }
-   return res.status(200).json({blog})
-}
+
