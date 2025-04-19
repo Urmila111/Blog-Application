d1b99eb077c686fadc4e16a3ac5bb268678f8b0a
Urmila111 <basneturmi777@gmail.com>
2025-04-19 21:47:03 +0545

    updated blog controller

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -60,4 +60,20 @@ export const getById = async(req, res, next)=> {
       return res.status(404).json({message: "No Blog Found"})
     }
     return res.status(200).json({blog})
+ };
+
+ export const deleteBlog = async(req, res, next) =>{
+    const id = req.params.id;
+
+    let blog; 
+    try {
+        blog = await Blog.findByIdAndRemove(id)
+    } catch (err) {
+        console.log(err);
+    }
+    if (!blog){
+     return res.status(400).json({message: "Unable to delete"})
+    }
+    return res.status(200).json({message: "Successfully deleted"})
  }
+
