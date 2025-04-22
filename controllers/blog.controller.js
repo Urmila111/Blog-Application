fb32ad5968089d070fd588da4a604d5b4723fd8c
Urmila111 <basneturmi777@gmail.com>
2025-04-22 14:49:32 +0545

    dd controller to get blogs by user ID with population

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -98,5 +98,19 @@ export const getById = async(req, res, next)=> {
      return res.status(400).json({message: "Unable to delete"})
     }
     return res.status(200).json({message: "Successfully deleted"})
+ };
+
+ export const getByUserId = async (req, res, next) => {
+    const userId = req.params.id;
+    let userBlogs;
+    try {
+        userBlogs = await User.findById(userId).populate("blogs");
+    } catch (err) {
+        return console.log(err)
+    }
+    if (!userBlogs){
+        return res.status(404).json({message: "No Blogs Found"})
+    }
+    return res.status(200).json({blogs:userBlogs})
  }
 
