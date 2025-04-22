5d94761578f3dc21a210b99b945b076be17ecdbf
Urmila111 <basneturmi777@gmail.com>
2025-04-22 12:59:17 +0545

    add deleteBlod function

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,20 +1,23 @@
 import {Blog} from "../models/blog.models.js";
+import {User} from "../models/user.models.js";
+import mongoose from "mongoose";
 
 export const getAllBlogs = async(req,res,next) => {
-    let blogs;
+    let blog;
     try {
-        blogs = await Blog.find();
+        blog = await Blog.find();
     } catch (err) {
         return console.log(err)
     }
-    if (!blogs){
+    if (!blog){
         return res.status(400).json({message:"No Blogs Found"})
     }
-    return res.status(200).json({blogs})
+    return res.status(200).json({blog})
 };
 
 export const addBlog = async(req,res,next) => {
     const { title, description, image, user} = req.body;
+   
     let existingUser;
     try {
         existingUser = await User.findById(user);
@@ -30,7 +33,8 @@ export const addBlog = async(req,res,next) => {
         image,
         user,
     });
-    try {
+
+        try {
        const session = await mongoose.startSession()
        session.startTransaction();
        await blog.save({session});
@@ -63,11 +67,12 @@ export const updateBlog = async(req,res,next) => {
     }
     return res.status(200).json({ blog });
 };
+
 export const getById = async(req, res, next)=> {
-    const id = req.params.id;
+    const blogId = req.params.id;
     let blog;
     try {
-       blog = await Blog.findById(id);
+       blog = await Blog.findById(blogId);
     } catch (err) {
       return console.log(err);
     }
@@ -78,11 +83,14 @@ export const getById = async(req, res, next)=> {
  };
 
  export const deleteBlog = async(req, res, next) =>{
-    const id = req.params.id;
+    const blogId = req.params.id;
 
     let blog; 
     try {
-        blog = await Blog.findByIdAndRemove(id)
+        blog = await Blog.findByIdAndDelete(blogId).populate("user");
+        await blog.user.blogs.pull(blogId);
+        await blog.user.save();
+        await Blog.findByIdAndDelete(blogId);
     } catch (err) {
         console.log(err);
     }
